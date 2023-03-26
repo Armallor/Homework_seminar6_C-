c77d40405a5776fd687163a2832b6e8589f827e6
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-26 16:05:46 +0300

    Ура, координаты

---
+++ Quest_43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+// Метод ввода значений
+
+int vvod (string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine()!);
+}
+
+// Метод определения координат точек пересечения
+
+string rezult (int x1, int y1, int x2, int y2, string text)
+{
+  string text1 = new string("Прямые совпадают");
+  string text2 = new string("Прямые параллельны");
+  string text3 = new string($"Прямые пересекаются в точке с координатами X = {(y2-y1) / (x1-x2)} и Y = {(x2 * y1 - x1 * y2) / (x2 - x1)}");
+  if ((x1 == x2) & (y1 == y2)) text = text1;
+  else if (x1 == x2) text = text2;
+  else text = text3;
+return text;
+}
+
+// Основные действия
+
+int k1 = vvod("Введите значение коэффициента k1 ");
+int b1 = vvod("Введите значение коэффициента b1 ");
+int k2 = vvod("Введите значение коэффициента k2 ");
+int b2 = vvod("Введите значение коэффициента b2 ");
+
+string notext = new string("");
+
+string result = rezult(k1, b1, k2, b2, notext);
+Console.Write($"{result}");
