9a2b77a42c6c088939f0c9359847c4bb94a28835
Danil Muzafarov <d.muzafarov95@mail.ru>
2023-03-26 15:06:27 +0300

    Решение задачи на количество положительных чисел

---
+++ Quest_41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+// Методы
+
+// Метод считывания массива с клавиатуры ввода
+
+string[] ReadingText ()
+{
+Console.Write("Введите числа через запятую и пробел: ");
+string[] array = Console.ReadLine()!.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+return array;
+}
+
+// Метод определения количества положительных чисел
+
+int quantity (string[] array)
+{int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (int.Parse(array[i]) > 0) count++;
+}
+return count;
+}
+
+// Основные действия
+
+string[] number = ReadingText();
+
+int quant = quantity(number);
+
+Console.Write($"Количество положительных чисел равно {quant}");
